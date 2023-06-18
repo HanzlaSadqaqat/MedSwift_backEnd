a274a7afb31ecd95a03a9105016053a335157fde
Hanzla Sadaqat <hanzlasadaqatrajput@gmail.com>
2023-06-18 03:29:02 -0700

    chore: updated commands

--- src/middleware/authenticateToken.ts
+++ src/middleware/authenticateToken.ts
@@ -11,7 +11,7 @@ const validateAccessToken = async (req: Request, res: Response, next: NextFuncti
     }
     if (token) {
       token = token.split(' ')[1]
-      let data = Jwt.verify(token, process.env.SECRET_KEY as string) as jwtInfo
+      let data = Jwt.verify(token, process.env.ACCESS_SECRET_KEY as string) as jwtInfo
       let user = await User.findOne({ email: data.email })
       if (!user) {
         throw 'Unauthorized'
